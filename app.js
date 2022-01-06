9be1fe302bef4f5eb14de863e07b1d81e8da57b3
subrata797979 <subrata797979@gmail.com>
2022-01-07 03:03:46 +0530

    database moved to cloud

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected To Database!'))
